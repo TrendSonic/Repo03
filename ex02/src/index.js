dbe91d7087df3547395e6672d972bc70a7d7d094
Edin <edinosmic77@gmail.com>
2021-09-16 00:57:53 +0200

    adding ex02 to Repo03

---
+++ ex02/src/index.js
@@ -0,0 +1,19 @@
+function main() {
+    //Only change code below this line
+
+    // This is a single line JS comment
+
+    /* This is a multi-line JS comment!
+    This is a multi-line JS comment!
+    This is a multi-line JS comment!
+    This is a multi-line JS comment!
+    This is a multi-line JS comment!
+    */
+
+    //Only change code above this line
+
+}
+
+console.log(main());
+
+module.exports = main;
