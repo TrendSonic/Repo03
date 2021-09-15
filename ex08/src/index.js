db753b8a696a02761d783ce51bf3d115f8da1af7
Edin <edinosmic77@gmail.com>
2021-09-16 01:38:47 +0200

    adding ex08 to Repo03

---
+++ ex08/src/index.js
@@ -0,0 +1,16 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    
+    var sum;
+    var numOne;
+    var numTwo;
+
+    sum = numOne + numTwo;
+    
+    // Only change code above this line
+    return sum;
+}
+
+console.log(main(numOne, numTwo)); // change this line
+
+module.exports = main;
