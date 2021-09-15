6947faca04372bdfdf4b3d85f4e6a69452b122ff
Edin <edinosmic77@gmail.com>
2021-09-16 01:45:57 +0200

    adding ex10 to Repo03

---
+++ ex10/src/index.js
@@ -0,0 +1,16 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    
+    var product;
+    var numOne;
+    var numTwo;
+
+    product = numOne * numTwo;
+    
+    // Only change code above this line
+    return product;
+}
+
+console.log(main(numOne, numTwo)); // change this line
+
+module.exports = main;
